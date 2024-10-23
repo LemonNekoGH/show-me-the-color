a04364ea02584b3cd3f922e83689174d2b0b6cad
LemonNekoGH <self@lemonneko.moe>
2024-10-23 16:34:09 +0800

    ready to publish

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,39 @@
-const container = document.createElement('div')
-container.style.width = '100vw'
-container.style.height = '100vh'
-container.style.position = 'fixed'
-container.style.top = '0'
-container.style.left = '0'
-container.style.pointerEvents = 'none'
-container.id = 'color-preview-container'
-document.body.appendChild(container)
-
 const colorPreview = document.createElement('div')
 colorPreview.style.width = '40px'
 colorPreview.style.height = '20px'
-colorPreview.style.background = '#FF0000'
 colorPreview.style.position = 'absolute'
 colorPreview.style.top = '0'
 colorPreview.style.left = '0'
 colorPreview.style.zIndex = '9999'
+colorPreview.style.borderRadius = '4px'
+colorPreview.style.boxShadow = '0 0 5px rgba(0, 0, 0, 0.2)'
 colorPreview.id = 'color-preview'
 
-const debounced = <F extends (args: any) => any>(fn: F) => {
-    let timeout = 0
-    
-    return (args: any) => {
-        if (timeout) {
-            clearTimeout(timeout)
-        }
-        
-        timeout = setTimeout(() => {
-            fn(args)
-        }, 500)
-    }
-}
-
-let l = 0
-let t = 0
-
-const onSelectionChange = (_: Event) => {
-    if (colorPreview.isConnected)
-        colorPreview.remove()
-
+document.addEventListener('mouseup', () => {
     const selection = document.getSelection()
+    if (!selection || !selection.rangeCount) return
+    if (!selection.toString().trim()) return
 
-    if (!selection) return
-
-    colorPreview.style.top = `${t}px`
-    colorPreview.style.left = `${l}px`
+    const bounding = selection.getRangeAt(0).getBoundingClientRect()
+    colorPreview.style.top = `${bounding.bottom + 4}px`
+    colorPreview.style.left = `${bounding.right + 4}px`
     colorPreview.style.background = selection.toString()
-    
-    container.appendChild(colorPreview)
-}
 
-const onMouseMove = (e: MouseEvent) => {
-    l = e.clientX
-    t = e.clientY
-}
+    if (!colorPreview.style.background) {
+        return
+    }
 
-document.addEventListener('selectionchange', debounced(onSelectionChange))
-window.addEventListener('mousemove', onMouseMove)
+    document.body.appendChild(colorPreview)
+})
+
+document.addEventListener('selectionchange', () => {
+    const selection = document.getSelection()?.toString() ?? ''
+    if (selection || !colorPreview.isConnected) {
+        return
+    }
+
+    colorPreview.style.background = ''
+    colorPreview.remove()
+})
 
 console.log('Color previewer is running')
